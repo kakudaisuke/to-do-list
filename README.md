c3685c37cf4b7ef5dbd627ca3fc7f7509a1b22e9
kakudaisuke <dk.15step@gmail.com>
2020-10-31 22:29:08 +0900

    サイト初期構造作成（JS全然慣れないぜ、頑張るぜ！）

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # to-do-list
+
+# Objective
+自分に合ったTO DO リストが欲しい
+JavaScriptの練習
+
+# 要件
+使用言語：HTML, CSS, BootStrap, JavaScript, PHP
+
+# 作業工程
+HTML/CSSでダミーのビューを作る
+JSでタスクを表示できるようにする
+  ダミーのタスクのレコードを用意する
+押したら色が変わるようにする
+もう一度押したら色が戻るようにする
+タスクを記入し追加できるようにする
+
---
+++ js/app.js
@@ -0,0 +1,19 @@
+'use strict';
+{
+  //日付をアップデートする
+  window.onload = function () {
+    let today = new Date();
+    let dateToday = today.getDate();
+  }
+
+  document.getElementById('date-yesterday').textContent = "sam 31 octobre";
+  
+
+  const todos = [
+    {id: 0, title: 'tâche 1', isDone: false},
+    {id: 1, title: 'tâche 2', isDone: false},
+    {id: 2, title: 'tâche 3', isDone: true}
+  ];
+
+}
+
